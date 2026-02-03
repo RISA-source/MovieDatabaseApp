c0b127f6dfd3fbdff9e0495be52e40d0e286f054
Rishi Saraff <rishisaraffofficial@gmail.com>
2026-02-03 18:34:14 +0545

    Imp Change

--- config/database.php
+++ config/database.php
@@ -5,12 +5,6 @@ define('DB_NAME', 'movie_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-// College Database Configuration
-// define('DB_HOST', 'localhost');
-// define('DB_NAME', 'np03cs4a240092');
-// define('DB_USER', 'np03cs4a240092');
-// define('DB_PASS', 'DDT6D54YCm');
-
 try {
     $pdo = new PDO(
         "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4",
